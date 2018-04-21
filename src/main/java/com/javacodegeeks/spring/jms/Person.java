f715a05419aa880b3e98f9a19ad4358ddfdd048b
Anjan <anjanreddyp@gmail.com>
2018-04-21 13:09:20 -0500

    comiitting new changes

--- src/main/java/com/javacodegeeks/spring/jms/Person.java
+++ src/main/java/com/javacodegeeks/spring/jms/Person.java
@@ -14,6 +14,6 @@ public class Person {
 		return age;
 	}
 	public String toString() {
-		return "Person: name(" + name + "), age(" + age + ")";
+		return "Person: name(" + name new+ "), age(" + age + ")";
 	}
 }
